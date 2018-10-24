27924b6ee7c683536a4c4db6236ba1518faf9a88
Christopher Mühl <christopher@padarom.io>
2018-10-24 15:49:00 +0200

    Start plane with a nicer rotation

--- src/renderer/Renderer.js
+++ src/renderer/Renderer.js
@@ -1,11 +1,10 @@
 // @flow
 import * as THREE from 'three'
-
 export default function start (element: Element, width: number, height: number) {
     let scene = new THREE.Scene()
 
     let camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 )
-    camera.position.z = 100
+    camera.position.z = 80
 
     let ambient = new THREE.AmbientLight(0xbbbbbb)
     scene.add(ambient)
@@ -27,6 +26,8 @@ export default function start (element: Element, width: number, height: number) 
     renderer.setClearColor(0xfffff0, 1)
     renderer.setSize( width, height )
     element.appendChild( renderer.domElement )
+
+    plane.rotation.x = 2
 
     function animate() {
         requestAnimationFrame( animate )
